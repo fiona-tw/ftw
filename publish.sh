c1c93781951b12ef2df3af912f47b906454c6146
Fiona Tahta-Wraith <f.tahta-wraith@iwoca.co.uk>
2020-01-02 15:36:49 +0000

    test semi-auto publish script increment version on pypi

---
+++ publish.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# builds `dist/` according to `setup.py`
+python3 setup.py sdist bdist_wheel
+
+# publishes to pypi the version defined in `setup.py`
+# Note: Will error if this version already exists!
+python3 -m twine upload dist/*
